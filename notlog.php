f664fbad8243cad70f9a35866cc36a70924deca0
Tom APOSTIN <tapostin@e1r3p7.42.fr>
2016-08-15 17:18:59 +0200

    Yo

---
+++ notlog.php
@@ -0,0 +1,8 @@
+<?php
+	echo '<div><p>Connecte toi !!!!</p></div>';
+	echo '<script type="text/javascript">
+				setTimeout(function() {
+								window.location = "index.php"
+													},2000);
+	</script>';
+?>
